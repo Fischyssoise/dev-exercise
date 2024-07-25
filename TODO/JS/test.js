4906679143120b69fd2ed080e9c21b7f952a04d0
Fischyssoise <ajfisch013@gmail.com>
2024-07-25 16:47:33 -0500

    Add div creator and form visibility toggle

--- TODO/JS/test.js
+++ TODO/JS/test.js
@@ -6,8 +6,22 @@ function createTodo(todo) {
   todos.append(newTodo);
 }
 
+function createTodoDiv(todo) {
+  const newDiv = document.createElement("div");
+  newDiv.id = 'todoItem'
+  newDiv.className = 'list'
+  newDiv.innerHTML = todo;
+  todos.append(newDiv);
+}
+
 document.querySelector("#newTodo").addEventListener("submit", function (event) {
   event.preventDefault();
   const todoMessage = document.querySelector("#newTodo textarea").value;
-  createTodo(todoMessage);
+  createTodoDiv(todoMessage);
+  toggleForm();
+});
+
+document.querySelector("#newTodo").addEventListener("click", function (event) {
+  event.preventDefault();
+  toggleForm();
 });
