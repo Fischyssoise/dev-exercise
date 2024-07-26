0e695a4c54909d9f7696688afe70ce318d2c0e9d
Fischyssoise <ajfisch013@sbcglobal.net>
2024-07-26 16:19:37 -0500

    Consolidate todo scripts

--- TODO/JS/scripts.js
+++ TODO/JS/scripts.js
@@ -12,3 +12,21 @@ function toggleCardList() {
     viewToggle.classList.toggle(card);
 }
 
+//Create list item
+const todos = document.querySelector("ul#todos");
+
+function createTodo(todo) {
+  const newDiv = document.createElement("div");
+  newDiv.id = 'todoItem'
+  newDiv.classList.add("list");
+  newDiv.textContent = todo;
+  todos.append(newDiv);
+}
+
+document.querySelector("#newTodo").addEventListener("submit", function (event) {
+  event.preventDefault();
+  const inputField = document.querySelector("#description");
+  const todoMessage = inputField.value;
+  createTodo(todoMessage);
+  toggleForm();
+});
